7b168239099307dce18596de2db94f86dc3da5d3
Cassie <cassie.pericak@northwestern.edu>
2016-11-05 17:29:17 -0500

    updated pseudo code for js

---
+++ assets/javascript/index.js
@@ -0,0 +1,9 @@
+//load the page using document.ready
+//append the dom to list the 10 buttons using a for loop through the length of the number of buttons
+//capture the on click of each button using jquesry to query the giphy api for that search term 
+//use ajax request data
+//use .done to return data
+//display the still image
+//use an if statement to capture the on click of the still image and append the image to display the gif
+//pull the data from the user input box when the submit button is clicked
+//append the dom a new button with that name

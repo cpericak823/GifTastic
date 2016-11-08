77d544d647b102f4c36e141c0815ea4da29bfa42
Cassie <cassie.pericak@northwestern.edu>
2016-11-07 21:56:01 -0600

    updated buttons in jquery

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,53 +3,71 @@ $(document).ready(function() {
     //list your global variables:
 
     //create a variable that's set as an object 
-    var automobiles = [{
-        car: 'Porsche?'
+    var automobiles = {
+        cars: [{
+            car: 'Porsche'
 
-    }, {
-        car: 'ferrari'
+        }, {
+            car: 'Ferrari'
 
-    }, {
-        car: 'lamborghini'
+        }, {
+            car: 'Lamborghini'
 
-    }, {
-        car: 'aston martin'
+        }, {
+            car: 'Aston Martin'
 
-    }, {
-        car: 'range rover'
+        }, {
+            car: 'Range Rover'
 
-    }, {
-        car: 'tesla'
+        }, {
+            car: 'Tesla'
 
-    }, {
-        car: 'jaguar car'
+        }, {
+            car: 'Jaguar Car'
 
-    }, {
-        car: 'viper car'
+        }, {
+            car: 'Viper Car'
 
-    }, {
-        car: 'mercedes benz'
+        }, {
+            car: 'Mercedes Benz'
 
-    }, {
-        car: 'bmw'
+        }, {
+            car: 'BMW'
 
-    }];
-}
-
-
-//create a variable that will pull the category usind data- and set it as the query search term
-var category = $(this).data('category');
-
-//pull from the Giphy API using the variable category and set it as a variable
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + category + "&api_key=dc6zaTOxFJmzC&limit=10";
+        }]
+    };
 
 
-//append the dom to list the 10 buttons using a for loop through the length of the number of buttons
-//capture the on click of each button using jquesry to query the giphy api for that search term 
-//use ajax request data
-//use .done to return data
-//display the still image
-//use an if statement to capture the on click of the still image and append the image to display the gif
-//pull the data from the user input box when the submit button is clicked
-//append the dom a new button with that name
+    //create a variable that will pull the category usind data- and set it as the query search term
+    var category = $(this).data('category');
+
+    //pull from the Giphy API using the variable category and set it as a variable
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + category + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+
+    //append the dom to list the 10 buttons using a for loop through the length of the number of buttons
+    displayButtons();
+
+    function displayButtons() {
+        var buttonsHtml = "";
+        for (var i = 0; i < automobiles.cars.length; i++) {
+            var buttons = automobiles.cars[i];
+            var buttonStr = buttons.car;
+            buttonsHtml = buttonsHtml + '<div>' + buttonStr + '</div>';
+
+        }
+        $('#buttonCategory').html(buttonsHtml);
+        console.log(buttonsHtml);
+    }
+
+    //capture the on click of each button using jquery to query the giphy api for that search term 
+    //use ajax request data
+    //use .done to return data
+    //display the still image
+    //use an if statement to capture the on click of the still image and append the image to display the gif
+    //pull the data from the user input box when the submit button is clicked
+    //use .val().trim();
+    //append the dom a new button with that name
+    //use return false
+    //remove all your console.logs before submitting
 });
